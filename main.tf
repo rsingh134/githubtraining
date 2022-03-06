9c52dd430c855c2b611b139257b32482a20815a6
Singh <rajat.g.singh@accenture.com>
2022-03-06 15:27:30 +0530

    fist commit

--- main.tf
+++ main.tf
@@ -1,6 +1,11 @@
 provider "google" {
-  credentials = "file("SA-key1.json")"
+  credentials = "${file("SA-key1.json")}"
   project     = "development-342105"  // give your project-ID
   region      = "us-central1" // region where we’ll be working
   zone        = "us-central1-a" // availability zone
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "terraform-network"
+  auto_create_subnetworks = "false"
 }

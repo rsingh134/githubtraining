67dc28fd98949430aa7e4f8e1b90b33ac606ad74
Singh <rajat.g.singh@accenture.com>
2022-03-06 15:38:22 +0530

    branch commit

--- main.tf
+++ main.tf
@@ -4,3 +4,22 @@ provider "google" {
   region      = "us-central1" // region where we’ll be working
   zone        = "us-central1-a" // availability zone
 }
+
+resource "google_compute_instance" "default" {
+  name         = "test"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+}
